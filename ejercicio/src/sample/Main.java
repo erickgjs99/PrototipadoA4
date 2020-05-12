df0b19b0297da3a37b1adec0183f04737979ab15
erickgjs99 <erickjs99lol@gmail.com>
2020-05-12 17:19:17 -0500

    taller

---
+++ ejercicio/src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        int[] values = new  int[1000];
+
+        for (int i = 0; i < 1000; i++){
+            values[i] = (int) Math.random()*100;
+            System.out.println(values[i]);
+        }
+
+
+
+
+        for(int i = 0; i < 1000; i++){
+            int index = -1;
+            for(int j = 0; j < values.length; j++){
+                if (values[j] == 80){
+                    index = j;
+                }
+                break;}}}
+}
